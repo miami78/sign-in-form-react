3fe40ff9872f8c58fd58e14af8e34a760e4c6f16
Miami78 <miamilarry78@gmail.com>
2020-04-01 19:55:39 +0300

    Updates

---
+++ precache-manifest.9df25e2f3862b4cbbf18eae7c5ef9b46.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "808f46e0c5fc74aa99c5c6f0d5c0c3ae",
+    "url": "/sign-in-form-react/index.html"
+  },
+  {
+    "revision": "783333d78882ff7d86d9",
+    "url": "/sign-in-form-react/static/css/main.cee68987.chunk.css"
+  },
+  {
+    "revision": "1e93eaa769657bcc0100",
+    "url": "/sign-in-form-react/static/js/2.6b349be3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/sign-in-form-react/static/js/2.6b349be3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "783333d78882ff7d86d9",
+    "url": "/sign-in-form-react/static/js/main.270e416a.chunk.js"
+  },
+  {
+    "revision": "cbbceabd3f5914701eb0",
+    "url": "/sign-in-form-react/static/js/runtime-main.188dc139.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/sign-in-form-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
