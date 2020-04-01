159ee0502f669fb0cb06d5b2e1e48b3f09e8f729
Miami78 <miamilarry78@gmail.com>
2020-04-01 19:47:41 +0300

    add accecible-emoji

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -4,7 +4,9 @@ import './homepage.styles.scss';
 
 const HomePage = () => (
   <div className='homepage'>
-    <h1>THIS IS THE HOMEPAGE🤩🤩🥳🥳</h1>
+    <h1>THIS IS THE HOMEPAGE
+    <span role="img" aria-label="Snowman">🤩🤩🥳🥳</span>
+    </h1>
   </div>
 );
 
